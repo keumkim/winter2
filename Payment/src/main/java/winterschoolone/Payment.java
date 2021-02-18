f88b802e5dff1abb8a4c3d01aa2cf12fc28ca889
keumkim <77368991+keumkim@users.noreply.github.com>
2021-02-18 09:10:16 +0900

    Add files via upload

---
+++ Payment/src/main/java/winterschoolone/Payment.java
@@ -0,0 +1,89 @@
+package winterschoolone;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+
+@Entity
+@Table(name="Payment_table")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long orderId;
+    private String userId;
+    private String menuId;
+    private Integer qty;
+    private String cancelYn;
+
+    @PostPersist
+    public void onPostPersist(){
+        Payed payed = new Payed();
+        BeanUtils.copyProperties(this, payed);
+        payed.publishAfterCommit();
+        
+        try {
+                Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        } catch (InterruptedException e) {
+                e.printStackTrace();
+        }
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        Refunded refunded = new Refunded();
+        BeanUtils.copyProperties(this, refunded);
+        refunded.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(String menuId) {
+        this.menuId = menuId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+    public String getCancelYn() {
+        return cancelYn;
+    }
+
+    public void setCancelYn(String cancelYn) {
+        this.cancelYn = cancelYn;
+    }
+
+
+
+}
